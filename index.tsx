425a99b787124bcec9e6cbf5306a692f4d499036
Matthias Giger <i@matthiasgiger.com>
2020-09-09 21:38:25 +0200

    feat(setup): basic setup

---
+++ index.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { observable, action, Reaction } from 'mobx'
+import { observer } from 'mobx-react'
+import { createBrowserHistory } from 'history'
+import { parse, stringify } from 'query-string'
+
+const history = createBrowserHistory()
+
+export const Router = new (class {
+  pages = {}
+  @observable route
+  @observable parameters = {}
+
+  constructor() {
+    const { pathname, search } = history.location
+
+    const path = pathname.replace(/^\//, '')
+
+    if (path && path.length > 0) {
+      this.route = path
+    }
+
+    if (!search || search.length === 0) {
+      return
+    }
+
+    this.parameters = parse(search)
+  }
+
+  @action
+  go(route, parameters) {
+    this.route = route
+    this.parameters = parameters
+
+    history.push(`${route}?${stringify(parameters)}`)
+  }
+
+  @action
+  back() {
+    this.Page = null
+  }
+
+  @action
+  setPages(pages: { [key: string]: React.ReactNode }) {
+    this.pages = pages
+  }
+})()
+
+const Pages = observer(() => {
+  const { route, parameters } = Router
+
+  let Component = Overview
+
+  if (route === 'detail') {
+    Component = Detail
+  }
+
+  return <Component {...parameters} />
+})
